faf4e851a687f0ce02652f134187f339432eb033
MikhaelAdam <mikhaeladams737@gmail.com>
2026-06-12 20:54:27 +0700

    fix build scripts

--- build.bat
+++ build.bat
@@ -4,6 +4,7 @@ setlocal
 set CONFIG=%1
 if "%CONFIG%"=="" set CONFIG=Release
 
+
 if /I not "%CONFIG%"=="Debug" if /I not "%CONFIG%"=="Release" (
     echo Usage: build.bat [Debug^|Release]
     exit /b 1
@@ -16,12 +17,27 @@ if errorlevel 1 (
     exit /b 1
 )
 
+
 echo Building project (%CONFIG%)...
 cmake --build build --config %CONFIG%
 if errorlevel 1 (
     echo Build failed.
     exit /b 1
 )
-
+ 
 echo Build succeeded. Output is in build\%CONFIG%
+
+robocopy "build\_deps\flecs-build\%CONFIG%" "build\%CONFIG%" /E /Z /R:3 /W:5
+robocopy "build\_deps\sdl_image-build\%CONFIG%" "build\%CONFIG%" /E /Z /R:3 /W:5
+robocopy "build\_deps\sdl-build\%CONFIG%" "build\%CONFIG%" /E /Z /R:3 /W:5
+
+set EXE=build\%CONFIG%\RPGGame.exe
+if exist "%EXE%" (
+    echo Running %EXE% ...
+    "%EXE%"
+) else (
+    echo Could not find executable to run.
+    exit /b 1
+)
+
 endlocal
--- build.sh
+++ build.sh
@@ -15,3 +15,22 @@ echo "Building project ($CONFIG)..."
 cmake --build build --config "$CONFIG" -- -j"$(nproc)"
 
 echo "Build succeeded. Output is in build/"
+cp "./build/_deps/flecs-build/"$CONFIG"/flecs.dll" "./build/"$CONFIG"/"
+cp "./build/_deps/sdl_image-build/"$CONFIG"/SDL3_image.dll" "./build/"$CONFIG"/" 
+cp "./build/_deps/sdl-build/"$CONFIG"/SDL3.dll" "./build/"$CONFIG"/"
+
+if [[ "$RUNFLAG" == "run" ]]; then
+    EXE="build/RPGGame"
+    if [[ -f "build/$CONFIG/RPGGame" ]]; then
+        EXE="build/$CONFIG/RPGGame"
+    fi
+ 
+    if [[ -f "$EXE" ]]; then
+        echo "Running $EXE ..."
+        "$EXE"
+    else
+        echo "Could not find executable to run."
+        exit 1
+    fi
+fi
+ 
