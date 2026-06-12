d1a7154595f3343b4a50f4f16a04e6fd4d7ef5c4
MikhaelAdam <mikhaeladams737@gmail.com>
2026-06-12 23:48:44 +0700

    Added boilerplate

--- src/game.cpp
+++ src/game.cpp
@@ -1,2 +1,78 @@
 #include "game.hpp"
 
+Game::Game(int argc, char **argv)
+    : m_window(nullptr, &SDL_DestroyWindow),
+      m_renderer(nullptr, &SDL_DestroyRenderer)
+{
+}
+
+SDL_AppResult Game::Init(){
+    SDL_SetAppMetadata("RPG Game", "1.0.0", "com.mikhadams.rpggame");
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+	    return sdl_error();
+    }
+
+    m_window.reset(SDL_CreateWindow("RPG Game", 800, 600, SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN));
+    if (!m_window) {
+	    return sdl_error();
+    }
+
+    m_renderer.reset(SDL_CreateRenderer(m_window.get(), NULL));
+    if (!m_renderer.get())
+    {
+        return sdl_error();
+    }
+
+    if (!SDL_ShowWindow(m_window.get())) {
+	    return sdl_error();
+    }
+    
+    return SDL_APP_CONTINUE;
+
+}
+
+SDL_AppResult Game::HandleEvent(SDL_Event* event){
+    	switch (event->type)
+	{
+		case SDL_EVENT_QUIT:
+			return SDL_APP_SUCCESS;
+
+		case SDL_EVENT_WINDOW_CLOSE_REQUESTED:
+			if (event->window.windowID == SDL_GetWindowID(m_window.get())) {
+				return SDL_APP_SUCCESS;
+			}
+			break;
+
+		default:
+			return SDL_APP_CONTINUE;
+	}
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult Game::OnRender()
+{
+    SDL_SetRenderDrawColor(m_renderer.get(), 255, 0, 0, 255);
+    SDL_RenderClear(m_renderer.get());
+
+    SDL_RenderPresent(m_renderer.get());
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult Game::OnUpdate()
+{
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult Game::Iterate()
+{
+    if (const auto result = OnUpdate(); result != SDL_APP_CONTINUE) return result;
+    if (const auto result = OnRender(); result != SDL_APP_CONTINUE) return result;
+    return SDL_APP_CONTINUE;
+}
+
+void Game::Quit(SDL_AppResult result)
+{
+    SDL_DestroyRenderer(m_renderer.get());
+    SDL_DestroyWindow(m_window.get());
+    SDL_Quit();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,87 +1,33 @@
+#define SDL_MAIN_USE_CALLBACKS 1  
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
-#include <flecs.h>
-#include <imgui.h>
-#include <imgui_impl_sdl3.h>
-#include <imgui_impl_sdlrenderer3.h>
 
-int main(int argc, char* argv[]) {
-    // Initialize SDL3
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
-        SDL_Log("SDL_Init Error: %s", SDL_GetError());
-        return -1;
-    }
+#include "game.hpp"
 
-    // Create window
-    SDL_Window* window = SDL_CreateWindow("SDL3 + ImGui + CPM", 1280, 720, SDL_WINDOW_RESIZABLE);
-    if (!window) {
-        SDL_Log("Window creation failed: %s", SDL_GetError());
-        SDL_Quit();
-        return -1;
-    }
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv)
+{
+	const auto app = new Game(argc, argv);
+	*appstate = app;
+    return app->Init();
+}
 
-    // Create renderer
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
-    if (!renderer) {
-        SDL_Log("Renderer creation failed: %s", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return -1;
-    }
 
-    // Setup Dear ImGui context
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
+{
+	auto app = static_cast<Game*>(appstate);
+    return app->HandleEvent(event);
+}
 
-    // Setup Platform/Renderer backends
-    ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
-    ImGui_ImplSDLRenderer3_Init(renderer);
 
-    bool running = true;
-    while (running) {
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            // Forward events to ImGui
-            ImGui_ImplSDL3_ProcessEvent(&event);
-            
-            if (event.type == SDL_EVENT_QUIT) {
-                running = false;
-            }
-            if (event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED && event.window.windowID == SDL_GetWindowID(window)) {
-                running = false;
-            }
-        }
+SDL_AppResult SDL_AppIterate(void* appstate)
+{
+	auto app = static_cast<Game*>(appstate);
+    return app->Iterate();
+}
 
-        // Start the Dear ImGui frame
-        ImGui_ImplSDLRenderer3_NewFrame();
-        ImGui_ImplSDL3_NewFrame();
-        ImGui::NewFrame();
 
-        // Design your UI window here
-        ImGui::Begin("Hello, Framework!");
-        ImGui::Text("SDL3, ImGui, and CPM are working perfectly together.");
-        ImGui::End();
-
-        // Rendering setup
-        ImGui::Render();
-        SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
-        SDL_RenderClear(renderer);
-        
-        // Render UI over your scene
-        ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
-        SDL_RenderPresent(renderer);
-    }
-
-    // Cleanup routines
-    ImGui_ImplSDLRenderer3_Shutdown();
-    ImGui_ImplSDL3_Shutdown();
-    ImGui::DestroyContext();
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-    return 0;
+void SDL_AppQuit(void* appstate, SDL_AppResult result)
+{
+	auto app = static_cast<Game*>(appstate);
+	app->Quit(result);
 }
